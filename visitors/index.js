d845e046d62ac8f9eba550bc1c35f0cefb96a7e7
pentagramswheel <waquinofx@gmail.com>
2025-04-28 15:56:05 -0500

    Updated code to redis 4.7.0

--- visitors/index.js
+++ visitors/index.js
@@ -1,20 +1,23 @@
 const express = require("express");
 const redis = require("redis");
+const process = require("process");
 
 const app = express();
 const client = redis.createClient({
-  host: "redis-server",
-  port: 6379
-});
-client.set("visits", 0);
-
-app.get('/', (req, res) => {
-  client.get("visits", (err, visits) => {
-    res.send("Number of visits is " + visits);
-    client.set("visits", parseInt(visits) + 1);
-  });
+  url: "redis://redis-server:6379"
 });
 
-app.listen(6000, () => {
-  console.log("Listening on port 6000");
+(async () => {
+  await client.connect();
+  await client.set("visits", 0);
+})();
+
+app.get('/', async (req, res) => {
+  const visits = await client.get("visits");
+  res.send("Number of visits is " + visits);
+  await client.set("visits", parseInt(visits) + 1);
+});
+
+app.listen(8081, () => {
+  console.log("Listening on port 8081");
 });
